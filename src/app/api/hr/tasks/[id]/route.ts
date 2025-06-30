3fc346bbf3a91d2e450ccbdd6d204085e29a25e5
EzechielKiregha <ekiregha@gmail.com>
2025-06-30 19:27:04 +0200

    I am already in production for testing

--- src/app/api/hr/tasks/[id]/route.ts
+++ src/app/api/hr/tasks/[id]/route.ts
@@ -3,33 +3,39 @@ import prisma from '@/lib/prisma'
 import { authOptions } from '@/lib/auth'
 import { getServerSession } from 'next-auth'
 
-export async function GET(_:NextRequest,{params}:{params:{id:string}}){
+export async function GET(_:NextRequest,{ params }: { params: Promise<{ id: string }> }
+) {
+   const id = (await params).id; 
   const session = await getServerSession(authOptions);
           
         if (!session?.user?.companyId) {
           return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
         }
-  const t = await prisma.task.findUnique({ where:{id:params.id} })
+  const t = await prisma.task.findUnique({ where:{id} })
   return NextResponse.json(t)
 }
 
-export async function PUT(req:NextRequest,{params}:{params:{id:string}}){
+export async function PUT(req:NextRequest,{ params }: { params: Promise<{ id: string }> }
+) {
+   const id = (await params).id; 
   const session = await getServerSession(authOptions);
           
         if (!session?.user?.companyId) {
           return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
         }
   const data = await req.json()
-  const updated = await prisma.task.update({ where:{id:params.id}, data })
+  const updated = await prisma.task.update({ where:{id}, data })
   return NextResponse.json(updated)
 }
 
-export async function DELETE(_:NextRequest,{params}:{params:{id:string}}){
+export async function DELETE(_:NextRequest,{ params }: { params: Promise<{ id: string }> }
+) {
+   const id = (await params).id; 
   const session = await getServerSession(authOptions);
           
         if (!session?.user?.companyId) {
           return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
         }
-  await prisma.task.delete({ where:{id:params.id} })
+  await prisma.task.delete({ where:{id} })
   return NextResponse.json({ success:true })
 }
