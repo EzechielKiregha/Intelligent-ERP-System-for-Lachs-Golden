f29bf449aa94f491685785f6bb164f9b5e073f52
EzechielKiregha <ekiregha@gmail.com>
2025-06-30 17:55:00 +0200

    ALRIGHT

--- src/app/api/hr/documents/[id]/route.ts
+++ src/app/api/hr/documents/[id]/route.ts
@@ -3,13 +3,15 @@ import prisma from '@/lib/prisma'
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
-export async function GET(_:NextRequest,{params}:{params:{id:string}}){
+export async function GET(_:NextRequest,{ params }: { params: Promise<{ id: string }> }
+) {
+   const id = (await params).id;
   const session = await getServerSession(authOptions);
     
   if (!session?.user?.companyId) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
-  const d = await prisma.document.findUnique({ where:{id:params.id} })
+  const d = await prisma.document.findUnique({ where:{id} })
   return NextResponse.json(d)
 }
 
@@ -24,12 +26,14 @@ export async function PUT(req:NextRequest,{params}:{params:{id:string}}){
   return NextResponse.json(updated)
 }
 
-export async function DELETE(_:NextRequest,{params}:{params:{id:string}}){
+export async function DELETE(_:NextRequest,{ params }: { params: Promise<{ id: string }> }
+) {
+   const id = (await params).id;
   const session = await getServerSession(authOptions);
     
   if (!session?.user?.companyId) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
-  await prisma.document.delete({ where:{id:params.id} })
+  await prisma.document.delete({ where:{id} })
   return NextResponse.json({ success:true })
 }
